87072adfc5b45d05907c28c8b669915e4f5548d1
David Kayser <DavidKayser@users.noreply.github.com>
2022-01-28 13:02:09 -0800

    add arrays for story components

---
+++ script.js
@@ -0,0 +1,41 @@
+const subject = [
+    "David",
+    "Melissa",
+    "James",
+    "Harry",
+    "Jennifer",
+    "A man",
+    "A woman",
+    "The president",
+    "A clown",
+    "Will",
+    "Peter",
+    "A test pilot",
+    "A door to door fridge salesman",
+    "A nerd",
+    "A hero",
+    "A judge"
+];
+
+const action = [
+    "walked into a bar",
+    "ate a sandwhich",
+    "looked across the street",
+    "found a dollar",
+    "went to the mountains",
+    "flew to Alaska",
+    "formed a research group",
+    "ate some ramen",
+    "went out for sushi"
+]
+
+const end = [
+    "and died.",
+    "and had a geat time.",
+    "and went into hiding",
+    "and started a new company",
+    "and found the love of their life",
+    "and became a famous writer",
+    "and regretting it immediatly.",
+    "and became rich."
+]
